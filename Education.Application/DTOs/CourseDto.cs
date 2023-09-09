784b209b61a586fb3b8dc73c769e66983f1d3e16
deymerap <deymer.ap@gmail.com>
2023-09-09 17:17:32 -0500

    more

--- Education.Application/DTOs/CourseDto.cs
+++ Education.Application/DTOs/CourseDto.cs
@@ -14,6 +14,7 @@ namespace Education.Application.DTOs
         public DateTime? PublishedDate { get; set; }
         public Decimal Price { get; set; }
         public Decimal Hour { get; set; }
+        public Decimal HourOne { get; set; }
         public Decimal HourTow { get; set; }
     }
 }
